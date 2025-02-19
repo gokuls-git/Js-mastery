f5c0f3c7ec0355ac9cddc9b6d6cf847e93fccd4d
Gokul S <gokuls@Gokuls-MacBook-Air.local>
2025-02-19 22:50:57 +0530

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,4 +2,62 @@ let js = 'amazing';
 
 // if (js === 'amazing') alert('JavaScript is FUN!');
 
-console.log(40 * 2 + 5 - 3);
+// console.log(40 * 2 + 5 - 3);
+
+// Variable name conventions in javaScript
+
+let firstName = "Gokul";
+let myFirstJob = 'Associate UI Developer';
+let myCurrentJob = 'Frontend Developer';
+
+console.log( firstName,`my job is`, myFirstJob );
+
+
+let trueValue = true;
+console.log(true);
+
+
+// Dynamic Typing
+
+let year
+
+console.log(typeof firstName);
+console.log(typeof trueValue);
+console.log(typeof year);
+
+
+year = 1999;
+console.log(typeof year);
+
+// Basic Operators
+
+const now = 2025;
+const myAge = now - 1999;
+console.log(myAge * 2, myAge + 1, myAge / 2, myAge % 3, 2**2);
+
+const lastName = 'S';
+
+console.log(firstName + " " + lastName);
+
+// Assignment Operator
+
+let x = 15;
+x += 5;
+x++
+x--
+console.log(x);
+
+// Comparison Operators
+
+const isGreater = myAge > 26;
+const isGreaterOrEqual = myAge >= 26;
+
+console.log(isGreater);
+console.log(isGreaterOrEqual);
+
+// Operator Precedence
+
+
+
+
+
