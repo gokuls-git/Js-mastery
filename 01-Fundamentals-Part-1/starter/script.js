f0dfd9893e41b13b408af7698d1862cd5b321c54
Gokul S <gokuls@Gokuls-MacBook-Air.local>
2025-02-21 19:51:05 +0530

    type  conversion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,63 +1,149 @@
-let js = 'amazing';
+// let js = 'amazing';
 
-// if (js === 'amazing') alert('JavaScript is FUN!');
+// // if (js === 'amazing') alert('JavaScript is FUN!');
 
-// console.log(40 * 2 + 5 - 3);
+// // console.log(40 * 2 + 5 - 3);
 
-// Variable name conventions in javaScript
+// // Variable name conventions in javaScript
 
-let firstName = "Gokul";
-let myFirstJob = 'Associate UI Developer';
-let myCurrentJob = 'Frontend Developer';
+// let firstName = "Gokul";
+// let myFirstJob = 'Associate UI Developer';
+// let myCurrentJob = 'Frontend Developer';
 
-console.log( firstName,`my job is`, myFirstJob );
-
-
-let trueValue = true;
-console.log(true);
+// console.log( firstName,`my job is`, myFirstJob );
 
 
-// Dynamic Typing
-
-let year
-
-console.log(typeof firstName);
-console.log(typeof trueValue);
-console.log(typeof year);
+// let trueValue = true;
+// console.log(true);
 
 
-year = 1999;
-console.log(typeof year);
+// // Dynamic Typing
 
-// Basic Operators
+// let year
 
-const now = 2025;
-const myAge = now - 1999;
-console.log(myAge * 2, myAge + 1, myAge / 2, myAge % 3, 2**2);
+// console.log(typeof firstName);
+// console.log(typeof trueValue);
+// console.log(typeof year);
 
-const lastName = 'S';
 
-console.log(firstName + " " + lastName);
+// year = 1999;
+// console.log(typeof year);
+
+// // Basic Operators
+
+// const now = 2025;
+// const myAge = now - 1999;
+// console.log(myAge * 2, myAge + 1, myAge / 2, myAge % 3, 2**2);
+
+// const lastName = 'S';
+
+// console.log(firstName + " " + lastName);
 
 // Assignment Operator
 
-let x = 15;
-x += 5;
-x++
-x--
-console.log(x);
+// let x = 15;
+// x += 5;
+// x++
+// x--
+// console.log(x);
 
-// Comparison Operators
+// // Comparison Operators
 
-const isGreater = myAge > 26;
-const isGreaterOrEqual = myAge >= 26;
+// const isGreater = myAge > 26;
+// const isGreaterOrEqual = myAge >= 26;
 
-console.log(isGreater);
-console.log(isGreaterOrEqual);
+// console.log(isGreater);
+// console.log(isGreaterOrEqual);
 
 // Operator Precedence
 
 
 
+// TASK 1
+
+// let massMark = 75;
+// let heightMark = 1.69;
+
+// let massJohn = 86;
+// let heightJohn = 1.78;
 
 
+
+// let BMIMark = massMark / (heightMark ** 2);
+// let BMIJohn = massJohn / (heightJohn ** 2);
+
+// let BMI_result = BMIMark > BMIJohn;
+
+// console.log(BMIJohn +', '+  BMIMark, BMI_result );
+
+
+// Template Literals
+
+// const firstName = 'Gokul';
+// const Occupation = 'Software Developer';
+// const birthYear = 1999;
+// const currentYear = new Date().getFullYear();
+
+// const me = `My name is ${firstName} and i'am a ${Occupation} with age of ${currentYear - birthYear}`;
+// console.log(me);
+
+
+// Taking Decisions
+
+// const birthYear = 2010;
+// let age = new Date().getFullYear() - birthYear;
+
+// if (age >= 18) {
+//     console.log('Person is eligible for applying driving liscence');
+// }else{
+//     let yearsLeft = 18 - age
+//     console.log(`Person should wait for ${yearsLeft} years to apply for driving licence`);
+    
+// }
+
+
+// const birthYear = 1999;
+
+// if(birthYear <= 2000){
+//     console.log("Born in 20th century");
+// }else{
+//     console.log('Born in 21st century');
+    
+// }
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
+// }
+
+// Type Conversion and Coercion
+
+// + is used to concat two strings or values
+// on mathematic calculation it needed to be Number() / an numnber
+
+
+function abc (){
+    console.log(abc.xyz);
+
+}
+abc();
+
+abc.xyz = 400;
+abc.xyz = 200;
+
+abc();
+
+
+    
