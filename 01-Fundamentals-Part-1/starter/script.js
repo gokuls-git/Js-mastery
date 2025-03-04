5c0f282a0b3c7d58fb38e166ce8974f19b499bd9
gokuls-git <gokul.kumarrs7@gmail.com>
2025-03-04 19:00:43 +0530

    fundamental-2-init

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -98,7 +98,7 @@
 // }else{
 //     let yearsLeft = 18 - age
 //     console.log(`Person should wait for ${yearsLeft} years to apply for driving licence`);
-    
+
 // }
 
 
@@ -108,7 +108,7 @@
 //     console.log("Born in 20th century");
 // }else{
 //     console.log('Born in 21st century');
-    
+
 // }
 
 // const massMark = 78;
@@ -134,16 +134,112 @@
 // on mathematic calculation it needed to be Number() / an numnber
 
 
-function abc (){
-    console.log(abc.xyz);
+// function abc (){
+//     console.log(abc.xyz);
 
-}
-abc();
+// }
+// abc();
 
-abc.xyz = 400;
-abc.xyz = 200;
+// abc.xyz = 400;
+// abc.xyz = 200;
 
-abc();
+// abc();
 
 
-    
+// let scoreDolphins;
+// let scoreKoalas;
+// scoreDolphins = (96 + 18 + 89) / 3;
+// scoreKoalas = (88 + 91 + 110) / 3;
+
+// console.log(scoreDolphins, scoreKoalas);
+
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy!');
+
+// } else {
+//     console.log('No one wins trophy');
+
+// }
+
+
+// Switch Statements--
+
+// const day = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+// console.log(day);
+
+// switch (day) {
+//     case 'Monday':
+//         console.log("Start of the workweek! Stay motivated.");
+//         break;
+//     case 'Tuesday':
+//         console.log("Keep pushing forward.");
+//         break;
+//     case 'Wednesday':
+//         console.log("Midweek hustle! Almost there.");
+//         break;
+//     case 'Thursday':
+//         console.log("One more day to the weekend!");
+//         break;
+//     case 'Friday':
+//         console.log("Weekend is near! Finish strong.");
+//         break;
+//     case 'Saturday':
+//         console.log("Enjoy your weekend!");
+//         break;
+//     case 'Sunday':
+//         console.log("Relax and recharge for the week ahead.");
+//         break;
+//     default:
+//         console.log("Invalid day.");
+// }
+
+
+// Statement & Expression\
+
+// 4 + 3
+// 1999
+// true && false && !false
+
+// if(23 > 10){  // expression
+//     const x = 5;  // statements
+// }
+
+
+const me = 'Gokul';
+const dateOfBirth = 1999; // Declare with 'const' for consistency
+let today = new Date();
+
+let currentYear = today.getFullYear(); // Get the current year
+let age = currentYear - dateOfBirth; // Calculate age
+
+// console.log(today);
+// console.log(`My name is ${me}, I am ${age} years old.`);
+
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+
+
+
+// if(age >= 18){
+//     console.log(`you are ${age},eligible for voting.`)
+// }else{
+//     console.log(`not eligible to vote`);  
+// }
+
+// age >= 18 ? console.log(`you are ${age},eligible for voting.`) : console.log(`not eligible to vote`);
+
+
+////////////////////////////////////
+// Coding Challenge #4
+
+// const bill = 49;
+
+// const tip = bill > 300 ? bill * 0.20   : bill >= 50 && bill <= 300  ? bill * 0.15   : bill * 0.10;
+// const total = bill + tip;
+// console.log(`The bill was ${bill}.Rs ,the tip was ${tip}.Rs and total value is ${total}`)
